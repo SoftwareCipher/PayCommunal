218e35c6f58df8fb1e547904183c968b6629620c
Cipher <kirillkuznetsov181@gmail.com>
2022-08-02 19:30:37 +0300

    Initial commit

---
+++ src/main/java/com/pay/paycommunal/PayCommunalApplication.java
@@ -0,0 +1,13 @@
+package com.pay.paycommunal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayCommunalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PayCommunalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/pay/paycommunal/PayCommunalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pay.paycommunal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayCommunalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
