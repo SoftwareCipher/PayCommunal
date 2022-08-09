8403912a3c8378c1cb758943f6ddf730ddd0bf4d
Cipher <kirillkuznetsov181@gmail.com>
2022-08-09 16:20:50 +0300

    mini changes

--- src/main/java/com/pay/paycommunal/service/applicationProcessingLogic/PayCommunalProcessor.java
+++ src/main/java/com/pay/paycommunal/service/applicationProcessingLogic/PayCommunalProcessor.java
@@ -2,15 +2,12 @@ package com.pay.paycommunal.service.applicationProcessingLogic;
 
 import com.pay.paycommunal.entities.Pay;
 import com.pay.paycommunal.service.PayService;
-import com.pay.paycommunal.service.impl.PayServiceInterface;
 import com.pay.paycommunal.service.impl.StatusPay;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
-
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
